ccd9c37f0888344ac3824dfcefaeb693b563c12b
Geovanne Silva <geovanne.08@gmail.com>
2024-11-10 20:01:30 -0300

    lalalalala....asdasdasdas1

--- src/app.ts
+++ src/app.ts
@@ -2,28 +2,24 @@ import Express from 'express';
 import indexRoute from './routes';
 import cors from 'cors';
 
-// Initiate express
+// Inicia o Express
 const app = Express();
 
+// Configuração do middleware CORS
 app.use(cors({
   origin: '*', // Permite qualquer origem
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+  credentials: true // Se você precisar permitir cookies ou autenticação com CORS
 }));
 
 app.use(Express.json());
 
-// Setup “hello world” endpoint
+// Use as rotas
+app.use(indexRoute);
 
+// Inicia o servidor na porta especificada
 const port = process.env.PORT || 3000;
-app.options('*', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.status(200).send();
-});
-app.use(indexRoute)
-// Start the express server on the relevant port
 app.listen(port, () => {
-  console.log(`server is running on ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
