0a5b4d0c96b7c73bfd2309bb1303f784c18eedeb
Geovanne Silva <geovanne.08@gmail.com>
2024-11-17 18:35:34 -0300

    ssjdjsjsj

--- src/routes/gift.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const gift_controller_1 = require("../controllers/gift-controller");
-const giftRoute = (0, express_1.Router)();
-giftRoute.get('/gift', gift_controller_1.getGift);
-giftRoute.put('/gift/:id', gift_controller_1.updateGiftDisabled);
-exports.default = giftRoute;
